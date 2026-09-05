23f05a3c09274d7c6be9a6a3b0beebe92cc69f5b
btsi10.558 <btsi10.558@gmail.com>
2026-09-05 15:04:31 +0900

    feat: ContactController@indexを実装 (Phase2-13)

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Tag;
+
+class ContactController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::all();
+        $tags = Tag::all();
+
+        return view('contact.index', [
+            'categories' => $categories,
+            'tags' => $tags,
+        ]);
+    }
+}
