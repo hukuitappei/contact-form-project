5d7dda174676ea23575bfbcb7071d920f2078e56
btsi10.558 <btsi10.558@gmail.com>
2026-09-05 13:29:11 +0900

    feat: UserSeeder/CategorySeeder/TagSeederを実装 (Phase0-7)

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategorySeeder extends Seeder
+{
+    public function run(): void
+    {
+        $now = now();
+
+        $categories = [
+            '商品のお届けについて',
+            '商品の交換について',
+            '商品トラブル',
+            'ショップへのお問い合わせ',
+            'その他',
+        ];
+
+        foreach ($categories as $content) {
+            DB::table('categories')->insert([
+                'content' => $content,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ]);
+        }
+    }
+}
