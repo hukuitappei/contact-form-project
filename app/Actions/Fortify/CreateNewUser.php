8f6d2c95e85437c734b5720a455b92fb85a11aeb
btsi10.558 <btsi10.558@gmail.com>
2026-09-06 06:39:51 +0900

    feat: CreateNewUserアクションに日本語バリデーションメッセージを実装 (Phase3-20)
    - name/email/passwordの各バリデーションエラーに日本語メッセージを追加
    - passwordルールをPassword::default()からmin:8+confirmedに変更し、
      password.min/password.confirmedのメッセージ上書きを確実にした

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -11,8 +11,6 @@ use Laravel\Fortify\Contracts\CreatesNewUsers;
 
 class CreateNewUser implements CreatesNewUsers
 {
-    use PasswordValidationRules;
-
     /**
      * Validate and create a newly registered user.
      *
@@ -31,7 +29,14 @@ class CreateNewUser implements CreatesNewUsers
                 'max:255',
                 Rule::unique(User::class),
             ],
-            'password' => $this->passwordRules(),
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ], [
+            'name.required' => 'お名前を入力してください',
+            'email.required' => 'メールアドレスを入力してください',
+            'email.email' => 'メールアドレスはメール形式で入力してください',
+            'password.required' => 'パスワードを入力してください',
+            'password.min' => 'パスワードは8文字以上で入力してください',
+            'password.confirmed' => 'パスワードと一致しません',
         ])->validate();
 
         return User::create([
