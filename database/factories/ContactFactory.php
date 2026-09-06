f012b67f456082135c56868fa8ffa086ae74a742
btsi10.558 <btsi10.558@gmail.com>
2026-09-06 18:10:04 +0900

    test: 管理画面一覧・詳細のFeatureテストを実装 (Phase4-26)
    - ContactFactoryを追加
    - 一覧: 検索（keyword/gender/category_id/date）・7件ページネーション・認証ガードを検証
    - 詳細: 表示・存在しないIDの404・認証ガードを検証
    - 削除: レコード削除・リダイレクト・認証ガードを検証

---
+++ database/factories/ContactFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Contact>
+ */
+class ContactFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'category_id' => Category::factory(),
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'gender' => $this->faker->numberBetween(1, 3),
+            'email' => $this->faker->unique()->safeEmail(),
+            'tel' => $this->faker->numerify('###########'),
+            'address' => $this->faker->address(),
+            'building' => null,
+            'detail' => $this->faker->realText(50),
+        ];
+    }
+}
